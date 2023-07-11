e5ddbb62a4c452e254d5499bc33cca7a65be88f5
Nawal <nawalkavi@gmail.com>
2023-07-11 14:57:16 +0100

    Stacks

--- main.py
+++ main.py
@@ -1,82 +1,27 @@
-# Importing library and initialising.
-from pygame import *
-import random
-
-ylol = random.randint(1, 69)
-if ylol == 69:
-    while ylol == 69:
-        print("seggsee")
-init()
-
-# Setting up the screen.
-width = 1200
-height = 700
-mainWindow = display.set_mode((width, height))
-
-# Images.
-imageDictionary = {
-    "invader1": transform.scale(image.load("Images//Invader 1.png"), (40, 40)),
-    "invader2": transform.scale(image.load("Images//Invader 2.png"), (40, 40)),
-    "invader3": transform.scale(image.load("Images//Invader 3.png"), (40, 40)),
-    "player": transform.scale(image.load("Images//Player.png"), (40, 40))
-}
-
-invaderArray = []
+data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+top = -1
+pos = len(data) - 1
 
 
-class Object():
-    def __init__(self, objectName, sizeX, sizeY):
-        self.Size = [sizeX, sizeY]
-        self.CentrePosition = [sizeX / 2, sizeY / 2]
-        self.AbsolutePosition = [0, 0]
-        self.ObjectName = objectName
-
-    def editPosition(self, newX, newY):
-        self.AbsolutePosition[0] = newX - self.Size[0] / 2
-        self.AbsolutePosition[1] = newY - self.Size[1] / 2
-        self.CentrePosition[0] = newX
-        self.CentrePosition[1] = newY
+def pop():
+    global top, data
+    return data[top]
 
 
-class Invader(Object):
-    def __init__(self, objectName, sizeX, sizeY, invaderType):
-        self.invaderType = invaderType
-        super().__init__(objectName, 5, 20)
-        invaderArray.append(self)
+def push(item):
+    global top, data
+    top += 1
+    if top > (len(data) - 1):
+        print("ERROR")
+    else:
+        data[top] = item
+    return data[top]
 
 
-def generateInvader():
-    for row in range(0, 6):
-        for column in range(0, 17):
-            newInvader = Invader("Invader", 40, 40, "invader1")
-            newInvader.editPosition((column + 1) * 40, 75 + (row + 1) * 40)
-
-
-# Game loop.
-oneSecondTick = 0
-gameRunning = True
-
-generateInvader()
-print(invaderArray)
-while gameRunning == True:
-    for gameEvent in event.get():
-        # Quits the game by closing the window.
-        if gameEvent.type == QUIT:
-            gameRunning = False
-        # Handles keyboard actions.
-        if gameEvent.type == KEYDOWN:
-            if gameEvent.key == K_UP:
-                pass
-
-    # Fills the window with the specified colour (black).
-    mainWindow.fill((0, 0, 0))
-
-    for invaderValue in invaderArray:
-        mainWindow.blit(imageDictionary[invaderValue.invaderType],
-                        (invaderValue.AbsolutePosition[0], invaderValue.AbsolutePosition[1]))
-
-    # Game fps.
-    time.Clock().tick(144)
-
-    # Displays the output.
-    display.flip()
+print(top)
+push("Apple")
+push("melon")
+push("lemon")
+print(pop())
+print(pop())
+print(data)
